018725639c4a2f17881297f02f494a584f9672ff
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-01 00:05:32 +0500

    Task2

---
+++ Tyuiu.AntonovDI.Sprint1.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task2.V27.Lib;
+
+
+namespace Tyuiu.AntonovDI.Sprint1.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.CalculateSquarePerimetr(x);
+            Assert.AreEqual(8, res);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint1.Task2.V27/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task2.V27.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+        Console.WriteLine("Длина стороны квадрата:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Периметр квадрата -  " + ds.CalculateSquarePerimetr(x));
+        Console.ReadKey();
+    }
+}
