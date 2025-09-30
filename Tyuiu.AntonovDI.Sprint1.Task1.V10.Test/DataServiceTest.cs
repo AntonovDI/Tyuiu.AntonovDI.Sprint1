2905cb33b5dc66b451e42f38f63e7e48c80389bd
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-09-30 20:20:30 +0500

    Task1

---
+++ Tyuiu.AntonovDI.Sprint1.Task1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task1.V10.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint1.Task1.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.5, res);
+        }
+    }
+}
