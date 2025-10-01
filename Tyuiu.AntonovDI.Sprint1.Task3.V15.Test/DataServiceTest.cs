79a37a0a313dbfdb6ed7bad3c67d9f4b65e20664
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-01 21:38:52 +0500

    Task3

---
+++ Tyuiu.AntonovDI.Sprint1.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task3.V15.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint1.Task3.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double v1 = 3;
+            double v2 = 4;
+            double S = 15;
+            double T = 3;
+            var res = ds.DistanceOverTime(v1, v2, S, T);
+            Assert.AreEqual(36, res);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint1.Task3.V15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Intrinsics;
+using Tyuiu.AntonovDI.Sprint1.Task3.V15.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double v1; double v2; double S; double T;
+        Console.WriteLine("Введите скорость первого автомобиля: ");
+        v1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите скорость второго автомобиля: ");
+        v2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите начальное расстояние между автомобилями: ");
+        S = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите время потраченное на дорогу: ");
+        T = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine($"Итоговое расстояние между автомобилями - {Math.Round(ds.DistanceOverTime(v1, v2, S, T), 3)}");
+
+        Console.ReadKey();
+    }
+}
