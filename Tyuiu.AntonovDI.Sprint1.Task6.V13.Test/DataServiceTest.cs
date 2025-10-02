4f69c8e4a516a797e1dcc2c8a718642a7d8e9a87
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-02 23:41:50 +0500

    Task6

---
+++ Tyuiu.AntonovDI.Sprint1.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task6.V13.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint1.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string value = "abcd";
+            bool result = ds.CheckWordsAlphabet(value);
+            Assert.AreEqual(true, result);
+        }
+    }
+ }
---
+++ Tyuiu.AntonovDI.Sprint1.Task6.V13/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task6.V13.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу: пользователь вводит текст. Проверить, что буквы     *"); 
+        Console.WriteLine("* строки упорядочены по алфавиту.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string lines;
+        Console.WriteLine("Введите ваше слово, для проверки: ");
+        lines = Console.ReadLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (ds.CheckWordsAlphabet(lines) == true)
+        {
+            Console.WriteLine("В вашем слове все буквы упорядочены по алфавиту");
+        }
+        else
+        {
+            Console.WriteLine("В вашем слове буквы неупорядчены по алфавиту");
+        }
+    }
+}
