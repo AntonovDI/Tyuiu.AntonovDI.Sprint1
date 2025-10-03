e5c53613007d6ad24c5b1ba061ea160d1146a80a
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-03 23:54:01 +0500

    Task7

---
+++ Tyuiu.AntonovDI.Sprint1.Task7.V10/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.AntonovDI.Sprint1.Task7.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем. Ответ округлите   *");
+        Console.WriteLine("* округлите до 3 знаков после запятой. z=2ctg(3x)-(lncosx/ln(1+x^2))      *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x;
+        Console.WriteLine("Введите значение x (диапозон от -1 до 1): ");
+        x = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (1 >= x && x >= -1)
+        {
+            Console.WriteLine($"Ваш результат {ds.Calculate(x)}");
+        }
+        else
+        {
+            Console.WriteLine("Такое значение x недопустимо");
+        }
+    }
+}
